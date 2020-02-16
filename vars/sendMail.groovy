6b4c4734fa028ee42ca825f42fd0549d16d93275
NotaHacker1 <andrewsampson@hotmail.co.uk>
2020-02-16 19:05:06 +0000

    back to ext

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -2,8 +2,12 @@
 //@NonCPS
 def call(name, address) {
     println "in for ${name}";
-    [$class: 'Mailer', bcc: '', body: "Dear ${name} testy testy testington",
-    cc: '', from: 'swclpec@gmail.com', replyTo: 'swclpec@gmail.com',
-    subject: "test email from Jenkins",
-    recipients: "${address}"];
+    //[$class: 'Mailer', bcc: '', body: "Dear ${name} testy testy testington",
+    //cc: '', from: 'swclpec@gmail.com', replyTo: 'swclpec@gmail.com',
+    //subject: "test email from Jenkins",
+    //recipients: "${address}"];
+    emailext( body: "Dear ${name} testy testy testington", mimeType: 'text/html',
+        replyTo: 'swclpec@gmail.com', subject: "test email from Jenkins",
+        to: : "${address}"
+    )
 }
