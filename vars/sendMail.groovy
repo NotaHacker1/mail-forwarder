62fa7b7593ef0f3534bd8d3d90ad5cfdb921138a
NotaHacker1 <andrewsampson@hotmail.co.uk>
2020-02-16 18:34:24 +0000

    debug

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -1,6 +1,7 @@
 #!groovy
 //@NonCPS
 def call(name, address) {
+    println "in";
     mail bcc: '', body: "Dear ${name} testy testy testington",
     cc: '', from: 'swclpec@gmail.com', replyTo: 'swclpec@gmail.com',
     subject: "test email from Jenkins",
